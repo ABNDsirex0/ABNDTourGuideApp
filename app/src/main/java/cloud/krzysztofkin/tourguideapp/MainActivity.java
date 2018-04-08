82de1eb88cead9e5b527b88cb702ea8e6474445b
Krzysztof Kin <sirex0@gmail.com>
2018-04-08 17:31:15 +0200

    Rearrange menu

--- app/src/main/java/cloud/krzysztofkin/tourguideapp/MainActivity.java
+++ app/src/main/java/cloud/krzysztofkin/tourguideapp/MainActivity.java
@@ -1,16 +1,12 @@
 package cloud.krzysztofkin.tourguideapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
@@ -49,20 +45,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_city) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_attractions) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_data) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_app) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
